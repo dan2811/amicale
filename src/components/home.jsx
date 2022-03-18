c8a230b182189138b966670b43fb05b615484e23
Dan <djordandrums@gmail.com>
2022-03-18 09:53:48 +0000

    create navbar

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import HeaderBar from './headerbar';
+import NavBar from './navbar';
 
 const Wrapper = styled.div`
     font-family: 'Architects Daughter', cursive;
@@ -10,6 +11,7 @@ const Home = () => {
   return (
     <Wrapper>
         <HeaderBar />
+        <NavBar />
     </Wrapper>
   )
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import SchoolIcon from '@mui/icons-material/School';
+
+const Wrapper = styled.div`
+
+`;
+
+const Bar = styled.div`
+    position: absolute;
+    top: 5vh;
+    left: 0;
+    z-index: 99;
+    width: 100vw;
+    min-height: 5vh;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    box-shadow: 0px 0px 20px black;
+`;
+
+const Barright = styled.div`
+  flex: 7;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding-right: 2rem;
+`;
+
+const Barleft = styled.div`
+  flex: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 2rem;
+`;
+
+const MenuItem = styled(Link)`
+  color: black;
+  font-size: 1rem;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const MenuIcon = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 0.5rem;
+`;
+
+// const Barcenter = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+const Title = styled.h1`
+  color: black;
+  font-weight: 900;
+  font-size: 5rem;
+  padding: 0;
+  margin: 0;
+`;
+
+const NavBar = () => {
+  return (
+    <Wrapper>
+        <Bar>
+            <Barleft>
+              <Title>
+                Amicale
+              </Title>
+            </Barleft>
+            <Barright>
+              <MenuItem to="/">Home</MenuItem>
+              <MenuItem to="/">Mission</MenuItem>
+              <MenuItem to="/">What We Do</MenuItem>
+              <MenuItem to="/">Our Tutors</MenuItem>
+              <MenuItem to="/">Contact</MenuItem>
+              <MenuItem to="/">
+                <MenuIcon>
+                  <SchoolIcon />
+                </MenuIcon>
+                Book a Lesson
+              </MenuItem>
+              <MenuItem to="/">
+                <MenuIcon>
+                  <ShoppingCartIcon />
+                </MenuIcon>
+                Cart
+              </MenuItem>
+            </Barright>
+        </Bar>
+    </Wrapper>
+  )
+}
+
+export default NavBar
